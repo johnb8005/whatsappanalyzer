a0a09d84705e7517d459774ddee429787911b7a0
John B <john@nexys.ch>
2020-01-09 15:30:17 +0100

    deploy script

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,5 +1,4 @@
 yarn build
-SERVER_ADDRESS=83.166.144.235
-SERVER_USER=ubuntu
-
-scp -r build/* $SERVER_USER@$SERVER_ADDRESS:whatsappanalyzer
+git add -A
+git commit -a -m "new GH Pages version"
+git push origin master
