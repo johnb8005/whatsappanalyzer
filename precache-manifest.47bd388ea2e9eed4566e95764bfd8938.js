cbbcfaa651daeb743b982f982d3d80e468fe1898
johnb8005 <johnb8005@users.noreply.github.com>
2021-04-30 15:16:23 +0000

    deploy: 293dcd67f2aa645b4fb4bf82e5aef70ef9d93214

---
+++ precache-manifest.47bd388ea2e9eed4566e95764bfd8938.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee46437e8c1a12812e27ed7acca9fddf",
+    "url": "/whatsappanalyzer/index.html"
+  },
+  {
+    "revision": "bc53b5d98b15074b8604",
+    "url": "/whatsappanalyzer/static/css/main.c7d84c72.chunk.css"
+  },
+  {
+    "revision": "7d6ca62e93a6142ee5e7",
+    "url": "/whatsappanalyzer/static/js/2.b102dcc1.chunk.js"
+  },
+  {
+    "revision": "bc53b5d98b15074b8604",
+    "url": "/whatsappanalyzer/static/js/main.ece8bf49.chunk.js"
+  },
+  {
+    "revision": "f1ef9747c184a563c7ae",
+    "url": "/whatsappanalyzer/static/js/runtime~main.37f71523.js"
+  }
+]);
